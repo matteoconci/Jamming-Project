516c8172e3997bb33ec1e20cf0dfb4ac51a00aba
Matteo Conci <matteo.conci54@hotmail.it>
2025-03-05 11:52:13 +0100

    Add base track object to App, add base code of the tracklist component to receive the track object from the App component and display it in the tracklist component.

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -3,18 +3,57 @@ import './App.css'
 import Playlist from '../Playlist/playlist.jsx';
 import Tracklist from '../Tracklist/tracklist.jsx';
 
+const songArray = [
+  {
+      name: 'Endure',
+      artist: 'David A. Molina',
+      album: 'Genesis',
+      id: 1
+  },
+  {
+      name: 'Coma',
+      artist: 'Etsu',
+      album: 'Nightwalk',
+      id: 2
+  },
+  {
+      name: 'The Great Gig in the Sky',
+      artist: 'Pink Floyd',
+      album: 'The Dark Side of the Moon',
+      id: 3
+  },
+  {
+      name: 'Atrid',
+      artist: 'VonnBoyd',
+      album: 'Desperation',
+      id: 4
+  },
+  {
+      name: 'Serenity',
+      artist: 'The Pitcher',
+      album: 'Serenity/Our Core',
+      id: 5
+  },
+  {
+      name: 'Snow Flakes',
+      artist: 'Blackbird',
+      album: 'Lonely Bird',
+      id: 6
+  }
+];
 
 function App() {
 
   return (
     <>
       <Playlist />
-      <Tracklist />
       <div>
+        <input type="text" />
         <button>
           Search
         </button>
       </div>
+      <Tracklist tracks={songArray}/>
       <div >
         <button >
           Save to Spotify
--- src/Tracklist/tracklist.jsx
+++ src/Tracklist/tracklist.jsx
@@ -1,9 +1,34 @@
 import React from "react";
 import styles from './tracklist.module.css';
 
-function Tracklist() {
-    return  <h2 className={styles.tracklist}>Tracklist</h2>;
-   
+
+function Tracklist({tracks}) {
+    const resultsList = () => {
+        if(tracks.length > 0) {
+            return tracks.map((track) => (
+                <div key={track.id}>
+                    <h3>{track.name}</h3>
+                    <p>{track.artist}</p>
+                    <p>{track.album}</p>
+                </div>
+                )
+            );
+        } else {
+            return (
+                <div>
+                    <p>No results found</p>
+                </div>
+            );
+        };
+    };
+    return  (
+        <div>
+            <h2 className={styles.tracklist}>Tracklist</h2>
+            <div>
+                {resultsList()}
+            </div>
+        </div>    
+    );
 };
 
 export default Tracklist;
